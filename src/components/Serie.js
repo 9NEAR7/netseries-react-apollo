229266aa124d73213cdbc363959269f922caaca9
9NEAR7 <instantcrushxx97@gmail.com>
2022-06-15 20:16:07 -0500

    ultimas modificaciones a los estilos

--- src/components/Serie.js
+++ src/components/Serie.js
@@ -3,31 +3,28 @@ import React from 'react';
 const Serie = (props) => {
   const { serie } = props;
   return (
-    <div>
-      <div>
-        <b>{serie.name}</b><br></br> 
+    <div className='container-info'>
+      <div className='container-object'>
+        <b className='titulo'>{serie.name}</b><br></br> 
         <ul>            
             <li>
-                <b><span>Descripción</span></b><br></br>
-                {serie.description}
+                <b><span className='titulos2'>Descripción</span></b><br></br>
+                <p> </p><b className='pdes'>{serie.description}</b>
             </li>
             
             <li>
-                <b><span>Año de lanzamiento</span></b><br></br>
-                {serie.year}
+                <b><span className='titulos2'>Año de lanzamiento</span></b><br></br>
+                <b className='pdes'>{serie.year}</b>
             </li>
          
             <li>
-                <b><span>Temporadas</span></b><br></br>
-                {serie.seasons}
-            </li>
-
-            <li>
-                <b><span>Imagen</span></b><br></br>
-                <a href={serie.url}>{serie.url}</a>
-            </li>    
-
+                <b><span className='titulos2'>Temporadas</span></b><br></br>
+                <b className='pdes'>{serie.seasons}</b>
+            </li>   
         </ul>
+        <div className='image-container'>
+        <img src= {serie.url} className="imagenSerie"></img>
+        </div>
         
       </div>
     </div>
